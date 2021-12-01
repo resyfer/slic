d15c721759948fb35c86d45a4360289c05fa5bb5
resyfer <resyfer.dev@gmail.com>
2021-12-01 22:35:26 +0530

    added colored terminal output

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import (
+  "github.com/gookit/color"
+)
 
 func main() {
   traverseDirectory(".")
 
   for elem, val := range LANG {
     if val != 0 {
-      fmt.Printf("%v : %v%% \n", elem, float64(val) / float64(TOTAL_SIZE) * 100)
+      color.HEX(COLORS[elem]).Printf("%v : %v%% \n", elem, float64(val) / float64(TOTAL_SIZE) * 100)
     }
   }
 }
